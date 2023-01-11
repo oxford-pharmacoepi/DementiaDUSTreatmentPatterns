955d7605c0d353b8915e1fe25313ea970eb706bd
daniellenewby <danielle.newby@ndorms.ox.ac.uk>
2023-01-11 13:38:53 +0000

    update

--- RunAnalysis.R
+++ RunAnalysis.R
@@ -31,21 +31,67 @@ testy$demdustreatpats %>%
 
 #write.table(tsdsdf,file = here("1_InstantiateCohorts", "Cohorts", "input_cohorts.csv"))
 
-# set up treatment pattern settings
+if (!file.exists(output.folder)){
+  dir.create(output.folder, recursive = TRUE)}
 
-#create data settings (sets where the data is)
+if (!file.exists(plots.folder)){
+  dir.create(plots.folder, recursive = TRUE)}
+
+if (!file.exists(example.plots.folder)){
+  dir.create(example.plots.folder, recursive = TRUE)}
+
+# link to db tables -----
+person_db<-tbl(db, sql(paste0("SELECT * FROM ",
+                              cdm_database_schema,
+                              ".person")))
+observation_period_db<-tbl(db, sql(paste0("SELECT * FROM ",
+                                          cdm_database_schema,
+                                          ".observation_period")))
+visit_occurrence_db<-tbl(db, sql(paste0("SELECT * FROM ",
+                                        cdm_database_schema,
+                                        ".visit_occurrence")))
+condition_occurrence_db<-tbl(db, sql(paste0("SELECT * FROM ",
+                                            cdm_database_schema,
+                                            ".condition_occurrence")))
+drug_era_db<-tbl(db, sql(paste0("SELECT * FROM ",
+                                cdm_database_schema,
+                                ".drug_era")))
+concept_db<-tbl(db, sql(paste0("SELECT * FROM ",
+                               vocabulary_database_schema,
+                               ".concept")))
+concept_ancestor_db<-tbl(db, sql(paste0("SELECT * FROM ",
+                                        vocabulary_database_schema,
+                                        ".concept_ancestor")))
+
+death_db<-tbl(db, sql(paste0("SELECT * FROM ",
+                             cdm_database_schema,
+                             ".death")))
+
+
+# instantiate study cohorts ----
+info(logger, 'INSTANTIATING STUDY COHORTS')
+source(here("1_InstantiateCohorts","InstantiateStudyCohorts.R"))
+info(logger, 'GOT STUDY COHORTS')
+
+# set up treatment pattern settings --------------------------
+
+#create data settings (sets where the data is) with start and end dates
 dataSettings <- createDataSettings(OMOP_CDM = FALSE,
                                    cohortLocation = here("1_InstantiateCohorts", "Cohorts", "input_cohorts.csv")
                                   )
 
 
-#create cohort settings
+#create cohort settings ------------------
+
+# cohorts to create is the csv stating cohort ids and which cohort is the target (dementia) and event (anti dementia drugs)
+# cohorts folder is the table contain patient information with which cohort they are in and start and stop dates
 cohortSettings <-
   createCohortSettings( cohortsToCreate_location = here("1_InstantiateCohorts", "Settings", "cohorts_to_create.csv") ,
                         cohortsFolder = here("1_InstantiateCohorts", "Cohorts"))
 
 
 #create pathway settings
+<<<<<<< HEAD
 pathwaySettings <- createPathwaySettings(targetCohortId = 1, eventCohortIds = c(2, 3, 4, 5)) # use the defaults and seems to work due to change of names in older version of package
 
 #update the pathway results 
@@ -59,6 +105,13 @@ pathwaySettings$all_settings$analysis1[13] <- 5 #maxPathLength
 pathwaySettings$all_settings$analysis1[16] <- 100
 
 
+=======
+pathwaySettings <-
+  createPathwaySettings(
+    pathwaySettings_location = here("1_InstantiateCohorts", "Settings", "pathway_settings.csv")
+    
+  )
+>>>>>>> b4c440de3d44c259799e751aa78eda9b726641a3
 
 
 #save settings
@@ -110,6 +163,7 @@ saveSettings <- createSaveSettings(databaseName = db.name ,
                                    rootFolder = here("Results")
 )
 
+<<<<<<< HEAD
 TreatmentPatterns::executeTreatmentPatterns(dataSettings = dataSettings,
                                             cohortSettings = cohortSettings,
                                             pathwaySettings = pathwaySettings,
@@ -167,6 +221,18 @@ TreatmentPatterns::executeTreatmentPatterns(dataSettings = dataSettings,
 
 
 
+=======
+
+
+
+# getting original package to work
+
+# section B) referring to pre specified setting files in a folder - 
+
+dataSettings <- createDataSettings(OMOP_CDM = FALSE, 
+                                   cohortLocation = "C:/Users/dnewby/OneDrive - Nexus365/Desktop/treatmentpatterns_test/input_cohorts.csv")
+                                                           
+>>>>>>> b4c440de3d44c259799e751aa78eda9b726641a3
 
 
 
