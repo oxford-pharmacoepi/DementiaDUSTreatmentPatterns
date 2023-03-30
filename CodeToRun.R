bf0abdd4dad2edf07a0e9a1b6a29885f40146882
daniellenewby <danielle.newby@ndorms.ox.ac.uk>
2023-03-30 10:44:22 +0100

    update

--- CodeToRun.R
+++ CodeToRun.R
@@ -1,27 +1,18 @@
-renv::activate()
-renv::restore() # this should prompt you to install the various packages required for the study
-
-
 # packages -----
 # load the below packages 
-# you should have them all available, with the required version, after
-# having run renv::restore above
-library(DatabaseConnector)
-library(CirceR)
-library(CohortGenerator)
-library(here)
-library(stringr)
-library(tibble)
-library(dplyr)
-library(CDMConnector)
-library(RPostgres)
+remotes::install_github("darwin-eu-dev/TreatmentPatterns@stand-alone", force = TRUE)
 library(TreatmentPatterns)
+library(CohortGenerator)
+library(CirceR)
+library(dplyr)
+library(tools)
+library(here)
 
 # database metadata and connection details -----
 # The name/ acronym for the database
 db.name<-"..."
 
-output.folder <- here("...", db.name)
+output.folder <- here("Results", db.name)
 
 # database connection details
 server     <- "..."
@@ -31,7 +22,7 @@ password   <- "..."
 port       <- "..."
 host       <- "..."
 
-# driver for DatabaseConnector
+# connectionDetails
 downloadJdbcDrivers("...", here()) # if you already have this you can omit and change pathToDriver below
 connectionDetails <- createConnectionDetails(dbms = "...",
                                              server =server,
@@ -40,43 +31,27 @@ connectionDetails <- createConnectionDetails(dbms = "...",
                                              port = port ,
                                              pathToDriver = here())
 
-# sql dialect used with the OHDSI SqlRender package
-targetDialect <-"..."
-# schema that contains the OMOP CDM with patient-level data
-cdm_database_schema<-"..."
-# schema that contains the vocabularies
-vocabulary_database_schema<-"..."
-# schema where a results table will be created 
-results_database_schema<-"..."
+# cdm schema
+cdmDatabaseSchema <- "..."
 
-# stem for tables to be created in your results schema for this analysis
-# You can keep the above names or change them
-# Note, any existing tables in your results schema with the same name will be overwritten
-cohortTableStem<-"..." # needs to be in lower case
+# results schema
+resultSchema <- "..."
 
-#########################################
-## instantiating using CDM connector
-##########################################
-# instatiate the cohorts
-db <- DBI::dbConnect("...", dbname = server_dbi, port = port, host = host, user = user,
-                     password = password)
+#name of the cohort table
+cohortTable <- "..."
 
+#name of the study analysis
+studyname <- "..."
 
-# # cdm reference -----
-cdm <- CDMConnector::cdm_from_con(con = db, # connected using DBI dbConnect
-                                  cdm_schema = cdm_database_schema, #schema of the database
-                                  cdm_tables = tbl_group("clinical"), # which sets of tables needed
-                                  write_schema = results_database_schema) # need this to show where to write results to
+# instantiate cohorts for study
+source(here("InstantiateStudyCohorts.R"))
 
-
-
-
-
-
-# Run analysis ----
+#run study
 source(here("RunAnalysis.R"))
 
-# Review results -----
-TreatmentPatterns::launchResultsExplorer(saveSettings = saveSettings)
-TreatmentPatterns::launchResultsExplorer(output.folder = file.path(saveSettings$rootFolder, "output"))
-TreatmentPatterns::launchResultsExplorer(zipFolder = saveSettings$rootFolder)
+# luanch shiny
+launchResultsExplorer(saveSettings = saveSettings)
+
+# to save outputs
+saveAsFile()
+
